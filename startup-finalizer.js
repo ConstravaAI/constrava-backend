0fad74c2d76eb0aaba30f7ae96c72c99143e83f8
ConstravaAI <constravaai@gmail.com>
2026-07-03 09:19:22 -0400

    Run account auth guard fix during startup

--- startup-finalizer.js
+++ startup-finalizer.js
@@ -1,2 +1,4 @@
-const finalPatch = "./" + "account-auth-isolation-patch.js";
-await import(finalPatch);
+const accountPatch = "./" + "account-auth-isolation-patch.js";
+const guardFix = "./" + "account-auth-route-guard-fix.js";
+await import(accountPatch);
+await import(guardFix);
